0c2d5cfe25184cc000b001f6543585ee1f1b8a36
spacrocket <95596717+SpacRocket@users.noreply.github.com>
2022-10-23 23:52:45 +0200

    More exercises

--- exercises/standard_library_types/iterators2.rs
+++ exercises/standard_library_types/iterators2.rs
@@ -3,8 +3,6 @@
 // can offer. Follow the steps to complete the exercise.
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 // Step 1.
 // Complete the `capitalize_first` function.
 // "hello" -> "Hello"
@@ -12,7 +10,13 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        Some(first) => {
+            let mut string = String::from(first.to_uppercase().next().unwrap());
+            for it in c{
+                string.push(it);
+            }
+            string
+        }
     }
 }
 
@@ -21,7 +25,14 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    let mut string: Vec<String> = vec![];
+    let iterator = words.iter();
+
+    for slice in iterator{
+        string.push(capitalize_first(slice));
+    }
+
+    string
 }
 
 // Step 3.
@@ -29,7 +40,11 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    let mut s = String::new();
+    for string_el in capitalize_words_vector(words){
+        s.push_str(&string_el);
+    }
+    s
 }
 
 #[cfg(test)]
